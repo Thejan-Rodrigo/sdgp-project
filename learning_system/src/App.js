3144ab91b7900413872426d1702ca4fd20f8a35a
Achira-Prabhasha <prabhasha890@gmail.com>
2025-01-27 20:34:58 +0530

    create q and A
    Side navigation bar

--- learning_system/src/App.js
+++ learning_system/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Home from './components/homePage/Home'
+import QAndA from './components/q&a/QAndA';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <QAndA/>
     </div>
   );
 }
