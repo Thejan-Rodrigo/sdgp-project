896f65ebc15480e46fa8e29a6f7007e63f001e15
Achira-Prabhasha <prabhasha890@gmail.com>
2025-01-27 20:34:58 +0530

    create q and A
    Side navigation bar

---
+++ learning_system/src/components/q&a/QAndA.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function QAndA() {
+    return (
+        <div className="Q&A">
+            <div id="SideNav" class="SideNav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#">Meeting</a>
+                <a href="#">Student Profile</a>
+                <a href="#">Q&A</a>
+                <a href="#">Learning</a>
+                <a href="#">Announcement</a>
+            </div>
+        </div>
+    )
+}
