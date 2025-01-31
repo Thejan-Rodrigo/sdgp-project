90ce2b7169e2ae05f6110a38f2f45f10cfd68976
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-01-31 12:54:39 +0530

    Create Parent side
    Use Common nav Bar
    Create Parent Side Bar
    Update Meeting From

--- learning_system/src/components/meeting/ParentMessage.jsx
+++ learning_system/src/components/meeting/ParentMessage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function TeacherMassage(props) {
-    return (
-        <div class=" m-auto h-32 bg-white rounded-lg mb-10 p-3" style={{ width: 800 }}>
+export default function ParentMessage(props) {
+  return (
+    <div class=" m-auto h-32 bg-white rounded-lg mb-10 p-3" style={{ width: 800 }}>
             <div class="flex">
                 <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span class="sr-only">Open user menu</span>
@@ -18,11 +18,8 @@ export default function TeacherMassage(props) {
                 <button class=" text-white float-right w-10 h-7 border-2 border-green-500 rounded-lg font-extralight bg-green-400">
                     Join 
                 </button>
-                <button class=" text-white float-right w-16 h-7 border-2 border-red-400 rounded-lg font-extralight bg-red-500 mr-4">
-                    Remove  
-                </button>
             </div>
             
         </div>
-    )
+  )
 }
