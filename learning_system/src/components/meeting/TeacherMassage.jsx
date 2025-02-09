27c8f21ccaaa8fec0fe9598bcc4b1b45b2340bcd
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-02-09 20:12:02 +0530

    new meeting ui
    create a new ui for meeting page.

--- learning_system/src/components/meeting/TeacherMassage.jsx
+++ learning_system/src/components/meeting/TeacherMassage.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
+import { FaClock } from 'react-icons/fa';
 
 export default function TeacherMassage(props) {
     return (
-        <div class=" m-auto h-32 bg-white rounded-lg mb-10 p-3" style={{ width: 800 }}>
+        <div class=" ml-8 h-32 bg-white rounded-lg mb-10 p-3" style={{ width: 800 }}>
             <div class="flex">
-                <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo" />
-                </button>
+                
                 <h1 class="text-black font-semibold pl-4"> {props.name}</h1>
             </div>
-            <div class = "pt-1 font-normal">
+            <div class="pt-1 font-normal mb-5">
                 <p>{props.descrip}</p>
             </div>
-            <div >
-            
-                <button class=" text-white float-right w-10 h-7 border-2 border-green-500 rounded-lg font-extralight bg-green-400">
-                    Join 
-                </button>
-                <button class=" text-white float-right w-16 h-7 border-2 border-red-400 rounded-lg font-extralight bg-red-500 mr-4">
-                    Remove  
-                </button>
+            <div className="flex justify-between items-center w-full">
+                {/* Left side: Time with icon */}
+                <div className="flex items-center gap-2">
+                    <FaClock />
+                    <span>{props.time}</span>
+                </div>
+
+                {/* Right side: Buttons */}
+                <div className="flex items-center gap-2">
+                    <button className="text-white w-10 h-7 border-2 border-green-500 rounded-lg font-extralight bg-green-400">
+                        Join
+                    </button>
+                    <button className="text-white w-16 h-7 border-2 border-red-400 rounded-lg font-extralight bg-red-500">
+                        Remove
+                    </button>
+                </div>
             </div>
-            
+
         </div>
     )
 }
