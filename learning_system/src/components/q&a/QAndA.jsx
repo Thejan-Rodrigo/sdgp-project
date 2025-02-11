057f081c8a46e4b731fe55059f6c8f246157ba36
Achira <prabhasha890@gmail.com>
2025-02-11 15:06:23 +0530

    Chat Box
    Create chat box UI

--- learning_system/src/components/q&a/QAndA.jsx
+++ learning_system/src/components/q&a/QAndA.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
+import ChatBox from './ChatBox'
 
 export default function QAndA() {
     return (
-        <div className="Q&A">
-            <div id="SideNav" class="SideNav">
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                <a href="#">Meeting</a>
-                <a href="#">Student Profile</a>
-                <a href="#">Q&A</a>
-                <a href="#">Learning</a>
-                <a href="#">Announcement</a>
-            </div>
-        </div>
+        <ChatBox/>
     )
 }
