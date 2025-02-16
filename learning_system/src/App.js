c0f9657073a20a1afa8c06f636df788192283b79
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-02-16 10:48:24 +0530

    to rebase
    commit before rebase
    just install react router dom

--- learning_system/src/App.js
+++ learning_system/src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import Home from './components/homePage/Home'
 import LoginPage from './components/loginPage/LoginPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
