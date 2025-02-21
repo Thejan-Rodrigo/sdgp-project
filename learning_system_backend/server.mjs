ade76efb45e04ef3f60df2d09dcbe7ebe69bdc44
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-02-21 11:48:47 +0530

    get login
    rebase main to get the login part to the meeting branch
    fix some bugs when came with the rebase

--- learning_system_backend/server.mjs
+++ learning_system_backend/server.mjs
@@ -14,9 +14,8 @@ import progressRoutes from "./routes/progressRoutes.js";
 // import { getStudentProfile, getStudentProgress } from "./controllers/studentController.js";
 // import { getAttendance } from "./controllers/studentAttendace.js"
 import { getAuthUrl, handleOAuthCallback, generateMeetingLink } from './api/router/meeting.mjs';
-import connectDB from './config/db.mjs'// Import database connection
 import meetingRoutes from "./api/router/meetingRoutes.mjs";
-import cors from "cors";
+
 
 
 
@@ -54,17 +53,16 @@ app.use("/students", studentRoutes);
 //app.use("/attendance", attendanceRoutes);
 // app.use("/progress", progressRoutes);
 
-
 // Routes
 app.use("/api/students", studentRoutes);
 app.use("/api/progress", progressRoutes);
 connectDB();
 
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("MongoDB Connected with Mongoose!");
 });
 
-server.use("/api", meetingRoutes);
+app.use("/api", meetingRoutes);
 
 // Route to initiate OAuth2 flow
 app.get('/auth', (req, res) => {
