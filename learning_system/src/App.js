568c8929b599a59f546cec33a23720d3316fbd8d
Achira <prabhasha890@gmail.com>
2025-02-21 23:23:14 +0530

    Rebase and fixed error

--- learning_system/src/App.js
+++ learning_system/src/App.js
@@ -3,6 +3,7 @@ import AddAdminPage from './components/addAdmin/AddAdminPage';
 import Home from './components/homePage/Home'
 import LoginPage from './components/loginPage/LoginPage';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ParentChat from './components/q&a/ParentSide/ParentChat';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/addadmin" element={<AddAdminPage />}/>
+        <Route path="/parentq&a" element={<ParentChat/>}/>
       </Routes>
     </Router>
   );
