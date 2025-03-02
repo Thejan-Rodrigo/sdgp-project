b7cd777caa920744ffac5bfecbea19fa09398ca1
Kalin-Dissanayaka <kalinkavishka144@gmail.com>
2025-03-02 20:40:57 +0530

    changes in authentication and announcement

--- learning_system_backend/server.mjs
+++ learning_system_backend/server.mjs
@@ -22,6 +22,7 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 
 app.use('/api/v1', announcementRoute);
+app.use('/api/v1/announcements', announcementRoute);
 
 app.get("/test",(req,res)=>{
     console.log(req);
