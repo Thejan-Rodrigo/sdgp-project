bb99c3b9db815bd481a75cf6483e5ca9a225e819
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-03-07 18:00:17 +0530

    add logs
    add some logs

--- learning_system_backend/controllers/schoolController.js
+++ learning_system_backend/controllers/schoolController.js
@@ -1,14 +1,14 @@
 import catchAsync from "../utils/catchAsync.js";
 import { successResponse } from "../utils/responseHandler.js";
 import schoolService from "../services/schoolService.js";
+import logger from "../utils/logger.js";
 
 const schoolController = {
   createSchool: catchAsync(async (req, res) => {
-    console.log("Hello controller")
     const schoolData = req.body;
-    console.log(schoolData);
+    logger.info("[schoolController] Data came");
     const { school, admin } = await schoolService.createSchoolWithAdmin(schoolData);
-    
+    logger.info("[schoolController] School and admin created successfully");
     successResponse(res, { school, admin }, "School and admin created successfully", 201);
   }),
 };
