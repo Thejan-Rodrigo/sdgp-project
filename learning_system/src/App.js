a3a08822c9170ce4ed08ee8be4dd60f3066351e0
TariniApara <160596416+TariniApara@users.noreply.github.com>
2025-03-08 12:08:32 +0530

    add route path
    add route path for student profile

--- learning_system/src/App.js
+++ learning_system/src/App.js
@@ -3,6 +3,7 @@ import AddAdminPage from './components/addAdmin/AddAdminPage';
 import Home from './components/homePage/Home'
 import LoginPage from './components/loginPage/LoginPage';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StudentProfile from './components/studentProfilePage/studentProfilePage';
 
 const App = () => {
   const studentId = "650c7b3c8a2f8e12e45a1234"; // Replace with actual student ID from MongoDB
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/addadmin" element={<AddAdminPage />}/>
+        <Route path="/studentprofile" element={<StudentProfile />}/>
       </Routes>
     </Router>
   );
