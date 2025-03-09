c0843dac98d094e3de32fc57593a9c402edfff23
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-03-09 10:15:23 +0530

    update form
    update the form and fixed errors in the backend
    code in working stage

--- learning_system_backend/services/adminService.js
+++ learning_system_backend/services/adminService.js
@@ -1,11 +1,13 @@
 import User from "../models/User.js";
 import ApiError from "../utils/ApiError.js";
+import logger from "../utils/logger.js";
 
 const createAdmin = async ({ firstName, lastName, email, password, schoolId }) => {
   // Check if user already exists
+  logger.info("[createAdmin] searching a user")
   const existingUser = await User.findOne({ email });
   if (existingUser) throw new ApiError(400, "User with this email already exists");
-
+  logger.info("[createAdmin] user not found. Creating a new user")
   // Create new admin
   const admin = new User({
     firstName,
@@ -15,8 +17,10 @@ const createAdmin = async ({ firstName, lastName, email, password, schoolId }) =
     schoolId,
     role: "admin",
   });
+  logger.info("[createAdmin] user created successfully")
 
   await admin.save();
+  logger.info("[createAdmin] user save in the database successfully")
   return admin;
 };
 
