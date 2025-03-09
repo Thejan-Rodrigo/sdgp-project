2314304bbefaae742246726a3b2f5d77a9d0ed81
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-03-10 01:20:59 +0530

    Update apis
    add new api that delete Teacher By the teacher Id

--- learning_system_backend/routes/authRoutes.js
+++ learning_system_backend/routes/authRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 import authController from "../controllers/authController.js";
 
 // Access the methods from the imported authController
-const { login, register, getUsersBySchoolId } = authController;
+const { login, register, getUsersBySchoolId, deleteTeacherById } = authController;
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/users/:schoolId", getUsersBySchoolId); // New route
+router.get("/users/:schoolId", getUsersBySchoolId); // Get users by school ID
+router.delete("/teachers/:teacherId", deleteTeacherById); // Delete teacher by ID
 
 export default router;
