62bc24205b6e28fc0d1bc7477e4bee7f294f5e3b
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-03-09 17:00:07 +0530

    Update admin setting
    add delete admin by admin id
    update ui according to that

--- learning_system_backend/routes/adminRoutes.js
+++ learning_system_backend/routes/adminRoutes.js
@@ -10,4 +10,7 @@ router.post("/add", authMiddleware, adminController.addAdmin);
 // Route to get admins by school ID (Only accessible by super admins)
 router.get("/school/:schoolId", authMiddleware, adminController.getAdminsBySchoolId);
 
+// Route to delete an admin by ID (Only accessible by super admins)
+router.delete("/:adminId", authMiddleware, adminController.deleteAdminById);
+
 export default router;
