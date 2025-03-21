54b8212181aab4c84385d409af5807c683b83a1d
Achira <prabhasha890@gmail.com>
2025-03-22 00:56:43 +0530

    Add a contact list
    Add a contact list to teacher side, admin side and super admin side.

--- learning_system/src/components/q&a/AdminSide/AdminChat.jsx
+++ learning_system/src/components/q&a/AdminSide/AdminChat.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from '../Sidebar';
 import ChatAreaA from './ChatAreaA';
+import ContactListA from './ContactListA';
 
 const AdminChat = () => {
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
+      <ContactListA/>
       <ChatAreaA />
     </div>
   );
