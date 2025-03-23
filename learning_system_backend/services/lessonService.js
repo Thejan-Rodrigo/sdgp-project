8bf9c41f54b08451d1bb700c91500073ad1f48f7
Praveena0426 <praveenarathnayakework@gmail.com>
2025-03-23 23:36:54 +0530

    create the new lesson serivce

--- learning_system_backend/services/lessonService.js
+++ learning_system_backend/services/lessonService.js
@@ -2,16 +2,19 @@ import Lesson from "../models/Lesson.js";
 import ApiError from "../utils/ApiError.js";
 
 const addLesson = async ({grade, title, description, image }) =>{
-    // Add lesson logic here   
+    // Validate fields
     if(!grade || !title ||!description || !image){
         throw new ApiError(400, "All fields are required");
     } 
 
+    //exception handing
     try {
         const imageUrl = image; 
     
+        // create new lesson instance from the lesson model
         const newLesson = new Lesson({ grade, title, description, image: imageUrl });
     
+        //save the new lesson and return it
         await newLesson.save();
     
         return newLesson;
